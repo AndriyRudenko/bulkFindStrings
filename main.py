9f4aa29e4a276cbefe54537a1ca1e88954a8c709
AndriyRudenko <andriy.rudenko87@gmail.com>
2020-01-17 15:10:34 +0200

    add somethink

--- main.py
+++ main.py
@@ -7,10 +7,17 @@ def start_sort(path_to_template_file, path_to_folder):
     with open(path_to_template_file) as template_file:
         lines_template_file = [line_template_file.strip() for line_template_file in template_file]
     
+    for root, dirs, files in os.walk(path_to_folder):
+            for filename in sorted(files):
+                with open(path_to_folder + filename) as file:
+                  lines_file = [line_file.strip() for line_file in file]  
 
+                resault=set(lines_template_file) - set (lines_file)
 
-
-    
+                resault_file = open('resault_file', 'w')
+                resault_file.write (filename + "\n")
+                for item in resault:
+                    resault_file.write("%s\n" % item)
 
 
 def main():
@@ -28,12 +35,10 @@ def main():
     template_filename = input ("Enter the name of template file:\n")
 
     if path_to_folder[-1]!= "/":
-        path_to_template_file = path_to_folder + "/" + template_filename
-    elif path_to_folder[-1]== "/":
-        path_to_template_file = path_to_folder + template_filename
+        path_to_folder = path_to_folder + "/" 
+    
+    path_to_template_file = path_to_folder + template_filename
 
-    
-    
     if os.path.isfile(path_to_template_file)==True:
         start_sort (path_to_template_file, path_to_folder)
     else:
